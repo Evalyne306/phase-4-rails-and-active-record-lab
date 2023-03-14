8d14037541a5881bd3c72d1693ca0f582b5ff611
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2023-03-14 15:56:39 +0300

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: 'Jane',last_name: 'Doe', grade: 3)
+Student.create!(first_name: 'Anna',last_name: 'Smith', grade: 4)
+Student.create!(first_name: 'Pam',last_name: 'Ted',grade: 8)
